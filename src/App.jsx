0d094b8eddb95de4677c6a7e29b5d00c90df6e91
Christopher Rieder <christopher.rieder@gmail.com>
2019-09-10 18:35:50 -0300

    Scaffolding with empty files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function App () {
+  return (
+    <h1>Hello World!</h1>
+  );
+}
+
+export default connect(null)(App);
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import App from './App';
+import './styles/main.scss';
+import todoCreatorReducer from './TodoCreator/Reducer';
+import todoBoardReducer from './TodoBoard/Reducer';
+
+// const logger = createLogger();
+const rootReducer = combineReducers({
+  todoCreatorReducer: todoCreatorReducer,
+  todoBoardReducer: todoBoardReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>
+  ,
+  document.getElementById('root'));

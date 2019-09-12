699e374f3cff8dac0b900b4f234fc93ff59c63e6
Christopher Rieder <christopher.rieder@gmail.com>
2019-09-12 00:32:29 -0300

    styling

--- src/TodoBoard/TodoCardItem.jsx
+++ src/TodoBoard/TodoCardItem.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function TodoCardItem ({todo}) {
   return (
-    <div className='todoCardListItem'>
-      <p className='s'>{todo.title}</p>
-      <p className='s'>{todo.description}</p>
-      <p className='s'>{todo.author.name}</p>
-      <p className='s'>{todo.tags.join(', ')}</p>
+    <div className='todoCardItem'>
+      <p className='todoCardItem__title'>{todo.title}</p>
+      <p className='todoCardItem__description'>{todo.description}</p>
+      <p className='todoCardItem__author'>{todo.author.name}</p>
+      <p className='todoCardItem__tags'>{todo.tags.join(', ')}</p>
     </div>
   );
 }

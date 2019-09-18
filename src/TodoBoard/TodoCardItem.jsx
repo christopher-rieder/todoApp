107f40d2abe52fc6d370719e45dde1ec98808cee
Christopher Rieder <christopher.rieder@gmail.com>
2019-09-18 00:08:57 -0300

    fix: Do not display tag div if note has no tags

--- src/TodoBoard/TodoCardItem.jsx
+++ src/TodoBoard/TodoCardItem.jsx
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 import { filterByTag } from './Actions';
 
 function TodoCardItem ({todo, todoListStyle, children, dispatch}) {
+  const hasTags = !(todo.tags === [] || todo.tags[0] === '');
   return (
     <div className={'todoCardItem todoCardItem__' + todoListStyle}>
       <p className='todoCardItem__title'>{todo.title}</p>
       <p className='todoCardItem__description'>{todo.description}</p>
-      <div className='todoCardItem__tags'>{todo.tags.map(tag => <Chip key={tag} onClick={() => dispatch(filterByTag(tag))}>{tag}</Chip>)}</div>
+      {
+        hasTags &&
+        <div className='todoCardItem__tags'>{todo.tags.map(tag => <Chip key={tag} onClick={() => dispatch(filterByTag(tag))}>{tag}</Chip>)}</div>
+      }
       <div className='todoCardItem__buttons'>
         {children}
       </div>

b63e0f4684c4ade542b6bdb1b27db603d3f47898
Christopher Rieder <christopher.rieder@gmail.com>
2019-09-23 14:26:09 -0300

    adding spinner for loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import TodoBoard from './TodoBoard/TodoBoard';
+import Spinner from './SmallComponents/Spinner';
 import {requestTodos} from './TodoBoard/Actions';
+
+const mapStateToProps = state => ({
+  loadingState: state.apiReducer.todosListPending,
+  errorLoading: state.apiReducer.todosListError
+});
 
 function App ({loadingState, errorLoading, dispatch}) {
   useEffect(() => {
     dispatch(requestTodos());
   }, []);
 
+  if (loadingState) {
+    return <Spinner />;
+  }
+
+  if (errorLoading) {
+    return <p>ERROR: {errorLoading}</p>;
+  }
+
   return <TodoBoard />;
 }
 
-export default connect(null)(App);
+export default connect(mapStateToProps)(App);
--- src/api/Reducer.js
+++ src/api/Reducer.js
@@ -1,9 +1,31 @@
+import {
+  REQUEST_TODOS,
+  CHANGE_TODO_STATUS,
+  FILTER_BY_TAG
+} from '../TodoBoard/Actions';
+
+import {
+  ADD_TODO,
+  MODIFY_TODO
+} from '../TodoCreator/Actions';
+
 const initialState = {
+  todosListPending: true,
+  todosListError: '',
   apiCallPending: false,
   apiCallError: ''
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_TODOS.PENDING:
+      return {...state, todosListPending: true};
+    case REQUEST_TODOS.FAILED:
+      return {...state, todosListPending: false, todosListError: action.payload};
+    case REQUEST_TODOS.SUCCESS:
+      return {...state, todosListPending: false, todosListError: ''};
+  }
+
   if (action.type.endsWith('_PENDING')) {
     return {...state, apiCallPending: true};
   }
@@ -13,9 +35,10 @@ const reducer = (state = initialState, action) => {
   }
 
   if (action.type.endsWith('_SUCCESS')) {
-    return {...state, apiCallPending: false};
+    return {...state, apiCallPending: false, apiCallError: ''};
   }
 
+  // return the original state if there are no changes
   return state;
 };
 
